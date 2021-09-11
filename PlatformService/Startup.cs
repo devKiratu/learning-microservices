20982d6536fd45531d9c96edce320f6ed5f100d9
devKiratu <dev.kiratu@gmail.com>
2021-09-11 13:32:32 +0300

    register dbContext to use InMemoryDatabase

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -2,11 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PlatformService.Data;
+using PlatformService.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +29,10 @@ namespace PlatformService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<MainDbContext>(o =>
+            {
+                o.UseInMemoryDatabase(nameof(Platform));
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
