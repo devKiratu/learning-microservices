13f2cb0da4714f45c6bc44f603d423264e14772b
devKiratu <dev.kiratu@gmail.com>
2021-09-11 13:31:48 +0300

    add dbContext

---
+++ PlatformService/Data/MainDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options) {}
+
+        public DbSet<Platform> Platforms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //
+        }
+    }
+}
