15f67357000e925cbcf91f656140e9ff1b0e3279
devKiratu <dev.kiratu@gmail.com>
2021-09-13 18:13:19 +0300

    add controller for receiving data/comms from PlaformService

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/commands/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST command from platforms controller.");
+
+            return Ok("Inbound POST Command Service");
+        }
+    }
+}
