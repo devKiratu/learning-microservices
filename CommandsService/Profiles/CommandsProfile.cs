6a2c5419c534fda12fc39387d6da7197bbc0b850
devKiratu <dev.kiratu@gmail.com>
2021-09-17 15:16:40 +0300

    add and map dtos

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -11,6 +11,8 @@ namespace CommandsService.Profiles
             CreateMap<Platform, PlatformReadDto>();
             CreateMap<Command, CommandReadDto>();
             CreateMap<CommandCreateDto, Command>();
+            CreateMap<PlatformPublishedDto, Platform>()
+                .ForMember(dest => dest.ExternalId, o => o.MapFrom(src => src.Id));
         }
     }
 }
