63475c4f88951ada0640956aa207906a51d31d8e
devKiratu <dev.kiratu@gmail.com>
2021-09-20 11:21:50 +0300

    configure commands service to make grpc call to platform service on startup

---
+++ CommandsService/Data/PrepDb.cs
@@ -0,0 +1,38 @@
+﻿using CommandsService.Models;
+using CommandsService.SyncDataServices.Grpc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.Data
+{
+    public class PrepDb
+    {
+        public static void PopulateData(IApplicationBuilder builder)
+        {
+            using var scope = builder.ApplicationServices.CreateScope();
+            var provider = scope.ServiceProvider;
+            var grpcClient = provider.GetRequiredService<IPlatformDataClient>();
+            var platforms = grpcClient.ReturnAllPlatforms();
+
+            SeedData(provider.GetRequiredService<ICommandRepository>(), platforms);
+        }
+
+        private static void SeedData(ICommandRepository repository, IEnumerable<Platform> platforms)
+        {
+            Console.WriteLine("--> Seeding new platforms...");
+
+            foreach (var p in platforms)
+            {
+                if (!repository.ExternalPlatformExists(p.ExternalId))
+                {
+                    repository.CreatePlatform(p);
+                }
+                    repository.SaveChanges();
+            }
+        }
+    }
+}
--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -70,6 +70,8 @@ namespace CommandsService
             {
                 endpoints.MapControllers();
             });
+
+            PrepDb.PopulateData(app);
         }
     }
 }
